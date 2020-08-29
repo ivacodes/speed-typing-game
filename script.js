1c6844e3720b097946f2d227c533576d3968e4c0
Iva Bozic <ivabozic23@gmail.com>
2020-08-29 22:14:35 +0100

    working init

---
+++ script.js
@@ -0,0 +1,90 @@
+import { words } from "./words.js";
+
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreEl = document.getElementById("score");
+const timeEl = document.getElementById("time");
+const endGameEl = document.getElementById("end-game-container");
+const settingsBtn = document.getElementById("settings-btn");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
+const difficultySelect = document.getElementById("difficulty");
+
+// init
+let randomWord;
+let score = 0;
+let time = 10;
+
+let difficulty =
+  localStorage.getItem("difficulty") !== null
+    ? localStorage.getItem("difficulty")
+    : "medium";
+
+difficultySelect.value = difficulty;
+
+const timeInterval = setInterval(updateTime, 1000);
+
+//focus on input on reload
+text.focus();
+
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+function addWordToDOM() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + "s";
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    gameOver();
+  }
+}
+
+function gameOver() {
+  endGameEl.innerHTML = `
+  <h1>Time ran out</h1>
+  <p>Your finel score is ${score}</p>
+  <button onClick="location.reload()">Reload</button>`;
+  endGameEl.style.display = "flex";
+}
+
+addWordToDOM();
+
+text.addEventListener("input", (e) => {
+  const insertedText = e.target.value;
+
+  if (insertedText === randomWord) {
+    addWordToDOM();
+    updateScore();
+
+    e.target.value = "";
+
+    if (difficulty === "hard") {
+      time += 3;
+    } else if (difficulty === "medium") {
+      time += 4;
+    } else {
+      time += 6;
+    }
+
+    updateTime();
+  }
+});
+
+settingsBtn.addEventListener("click", () => settings.classList.toggle("hide"));
+
+settingsForm.addEventListener("change", (e) => {
+  difficulty = e.target.value;
+  localStorage.setItem("difficulty", difficulty);
+});
